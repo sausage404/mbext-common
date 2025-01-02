b81f1ed92247d5150a66a5c18ba5ad14043146f9
sausage404 <parinya24personal@gmail.com>
2025-01-02 11:52:33 +0700

    update

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -1,5 +1,5 @@
 export const dependencies = {
     modules: ['@minecraft/server', '@minecraft/server-ui', '@minecraft/server-net', '@minecraft/server-gametest'],
     plugins: ['@minecraft/vanilla-data'],
-    addons: ['@mcbedrock/database', '@mcbedrock/scoreboard', '@mcbedrock/fake-player']
+    addons: ['@mbext/database', '@mbext/scoreboard', '@mbext/fake-player']
 }
