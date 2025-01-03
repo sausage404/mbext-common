e96c21d7aa18e8cac4d52d64c62075e2f4c2a8ac
sausage404 <parinya24personal@gmail.com>
2025-01-03 17:28:50 +0700

    bump version to 1.0.0, add build scripts, update dependencies, and configure webpack

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -1,5 +1,5 @@
 export const dependencies = {
-    modules: ['@minecraft/server', '@minecraft/server-ui', '@minecraft/server-net', '@minecraft/server-gametest'],
-    plugins: ['@minecraft/vanilla-data'],
-    addons: ['@mbext/database', '@mbext/scoreboard', '@mbext/fake-player']
+    modules: ['@minecraft/server', '@minecraft/server-ui', '@minecraft/server-net', '@minecraft/server-admin', '@minecraft/server-gametest'],
+    plugins: ['@minecraft/vanilla-data', '@minecraft/math'],
+    addons: ['@mbext/database', '@mbext/command', '@mbext/scoreboard', '@mbext/fake-player']
 }
---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export * as constant from '../dist/constant';
+
+export namespace constant {
+    export const dependencies: {
+        modules: string[];
+        plugins: string[];
+        addons: string[];
+    };
+}
